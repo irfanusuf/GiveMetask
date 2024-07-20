67f648b5aa3f1b62a90ef31562e6e7bc3516ae45
unknown <irfanusuf33@gmail.com>
2024-07-20 12:55:05 +0530

    v1.1.5

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,47 +1,78 @@
 import axios from "axios";
 import React, { useState } from "react";
+import "../styles/signup.scss";
+import { ToastContainer, toast } from "react-toastify";
 
 const SignUp = () => {
-  const [username, setUsername] = useState("shoaib");
-  const [email, setEmail] = useState("shoaib@gmail.com");
-  const [password, setPassword] = useState("12345");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  //
-  const formDataArr = new FormData();
 
-  formDataArr.append("username", username);
-  formDataArr.append("email", email);
-  formDataArr.append("password", password);
+  // const formDataArr = new FormData();
+  // formDataArr.append("username", username);
+  // formDataArr.append("email", email);
+  // formDataArr.append("password", password);
 
-  const baseUrl = "http://localhost:5000";
-
+  const baseUrl = "http://localhost:4000";
   const handleSignup = async () => {
     try {
       // xml http post request
-      const res = await axios.post(`${baseUrl}/user/signup`, formDataArr);
-      console.log(res.message)
-
-
+      const res = await axios.post(`${baseUrl}/user/signup`, {
+        username,
+        email,
+        password,
+      });
+      toast.success(res.data.message);
     } catch (error) {
-      console.log(error);
+      console.log(error.message);
+      toast.error(error.message)
     }
   };
 
+  function handleClick() {
+    handleSignup();
+  }
 
-function handleClick (){
+  return (
+    <>
+      <ToastContainer />
+      <div className="signup">
+        <h1>Register With Us</h1>
 
-    handleSignup()
+        <form>
+          <input
+            placeholder="Enter your username "
+            type="text"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
 
-}
+          <input
+            placeholder="Enter your Email "
+            type="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
 
+          <input
+            placeholder="Enter your password "
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </form>
 
-
-  return <div>SignUp
-
-
-<button onClick={handleClick}> SignuP</button>
-
-  </div>;
+        <button onClick={handleClick}> Register</button>
+      </div>
+    </>
+  );
 };
 
 export default SignUp;
