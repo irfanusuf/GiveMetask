c11f195d40307cd0fe74d8726fbac6ec3fd88a64
Irfan usuf <irfanusuf33@gmail.com>
2024-09-03 10:56:48 +0530

    Update UserProfile.jsx

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import api from "../utils/AxiosInstance";
-const socket = io("http://localhost:4000");
+const socket = io("https://algoacademy.onrender.com");
 
 const UserProfile = (props) => {
 
